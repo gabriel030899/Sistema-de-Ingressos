d0ed333c150844701e345b7ff099e45ecce68da5
unknown <gabrieldoissteam@hotmail.com>
2019-11-18 02:32:22 -0200

    Alterei toda a estrutura do codigo para um sistema mais simples

--- sistema.c
+++ sistema.c
@@ -1,272 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//Funcao inicial do programa
 void main(){
-    // Variaveis
-    int ano;
-    int dia;
-    int mes;
-    int show;
-    int descontoTer = 0;
-    int ingressoTotal = 20;
-    int ingressoDesc = ingressoTotal-(ingressoTotal/2);
-
-    /* Mostrar que acabaram os assentos
-
-    int acabaramcadeiras = 0;
-    if(acabaramcadeiras == 1){
-        printf("\n\nNao ha mais assentos disponiveis!\n\n")
-    }
-
-    */
-
-    // Input do ano que o usuário deseja
-    printf("Digite o ano que deseja (2019/2020): ");
-    scanf("%d", &ano);
-    // 2019
-    if (ano == 2019){
-        // Input do dia que o usuário deseja
-        //Dezembro
-        printf("\nDezembro\n\nDigite o dia que deseja, exemplo(05): ");
-        scanf("%d", &dia);
-        if(dia == 3 || dia == 10 || dia == 17 || dia == 24 || dia == 31){
-            //dias para aplicar desconto de 100% para crianças
-            descontoTer = 1;
-        }
-        else if (dia <= 31 && dia >= 1){
-            //não aplicar desconto de 100% para crianças
-        }
-        else{
-            printf("Data invalida");
-        }
-    }
-    // 2020
-    else if (ano == 2020){
-        // Input do mes
-        printf("\n\nDigite o numero do mes que deseja, exemplo(01 = Janeiro): ");
-        scanf("%d", &mes);
-        switch(mes){
-            // Janeiro
-            //Input do dia
-            case 1:
-                printf("\nJaneiro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 7 || dia == 14|| dia == 21 || dia == 28){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Fevereiro
-            //Input do dia
-            case 2:
-                printf("\nFevereiro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 4 || dia == 11 || dia == 18 || dia == 25){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >=1 && dia <=29){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Março
-            //Input do dia
-            case 3:
-                printf("\nMarco\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 3 || dia == 10 || dia == 17 || dia == 24 || dia == 31){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Abril
-            //Input do dia
-            case 4:
-                printf("\nAbril\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 7 || dia ==14 || dia == 21 || dia == 28){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 30){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Maio
-            //Input do dia
-            case 5:
-                printf("\nMaio\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 5 || dia == 12 || dia == 19 || dia == 26){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Junho
-            //Input do dia
-            case 6:
-                printf("\nJunho\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 2 || dia == 9 || dia == 16 || dia == 23 || dia == 30){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 30){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Julho
-            //Input do dia
-            case 7:
-                printf("\nJulho\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 7 || dia == 14 || dia == 21 || dia == 28){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Agosto
-            //Input do dia
-            case 8:
-                printf("\nAgosto\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 4 || dia == 11 || dia == 18 || dia == 25){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Setembro
-            //Input do dia
-            case 9:
-                printf("\nSetembro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 1 || dia == 8 || dia == 15 || dia == 22 || dia == 29){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <=30){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Outubro
-            //Input do dia
-            case 10:
-                printf("\nOutubro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 6 || dia == 13 || dia == 20 || dia == 20 || dia == 27){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Novembro
-            //Input do dia
-            case 11:
-                printf("\nNovembro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 3 || dia == 10 || dia == 17 || dia == 24){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if(dia >= 1 && dia <= 30){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            // Dezembro
-            //Input do dia
-            case 12:
-                printf("\nDezembro\n\nDigite o dia que deseja, exemplo(05): ");
-                scanf("%d", &dia);
-                if (dia == 1 || dia == 8 || dia == 15 || dia == 22 || dia == 29){
-            //dias para aplicar desconto de 100% para crianças
-                descontoTer = 1;
-                }
-                else if (dia >= 1 && dia <=31){
-            //não aplicar desconto de 100% para crianças
-                }
-                else{
-                    printf("Data invalida");
-                }
-                break;
-            }
+    int op;
+    printf(" - Bem vindo! - \n\nSelecione o que deseja: \n1. Comprar ingressos\n2. Quit\n\nDigite: ");
+    scanf("%d", &op);
+    if(op == 1){
+        return data();
     }
     else{
-        printf("Ano invalido");
+        exit(2);
     }
-    // Input do espetaculo
-    printf("\n\nEspetaculos disponiveis: \n\n1. A Branca de Neve - as 16:00h\n2. Peter Pan - as 18:00h\n3. O Rei Leao - as 20:00h\n\nEscolha o espetaculo que deseja digitando seu numero: ");
-    scanf("%d", &show);
-    switch(show){
-        case 1:
-            printf("\nA Branca de Neve");
-            break;
-        case 2:
-            printf("\nPeter Pan");
-            break;
-        case 3:
-            printf("\nO Rei Leao");
-            break;
-        default:
-            printf("\nEspetaculo invalido");
-    }
-
-/*
-    if (descontoTer == 1){
-        printf("\n\nTem desconto\n\n");
-    }
-    else{
-        printf("\n\nNao tem desconto\n\n");
-    }
-*/
-// CRIAR O PAGAMENTO, ESCOLHA DE ASSENTOS E CAIXA
-
+    return 0;
 }
+//Variavel Global para verificar se eh Terca-feira ou nao para aplicar o desconto de 100% de criancas carentes da rede publica de ensino.
+int descontoTer=0;
+//Funcao da escolha da data do usuario
+int data(){
+    int dia;
+    system("cls");
+
+    printf("Dias da semana: \n\n1. Domingo\n2. Segunda-feira\n3. Terca-feira\n4. Quarta-feira\n5. Quinta-feira\n6. Sexta-feira\n7. Sabado\n\nEscolha o dia que deseja: ");
+    scanf("%d", &dia);
+    switch(dia){
+        case 1:
+            system("cls");
+            printf(" - Domingo - ");
+            return compras();
+            break;
+    }
+}
+// Variaveis Globais de compras para usar na variavel totalIngressos.
+int ingInteiro;
+int ingMeia;
+int ingTer;
+char nome[15];
+char email[50];
+// Variavel global para saber o total de ingressos comprados.
+int totalIngressos;
+//Funcao de compra de ingresso
+int compras(){
+
+    printf("\n\n - Preencha o cadastro para efetuar a compra: \n\nPrimeiro Nome: ");
+    scanf("%s", nome);
+    printf("\nE-mail que sera enviado(s) o(s) ingresso(s): ");
+    scanf("%s", email);
+
+    printf("\n\n - Tipos de ingressos:\n(Os documentos serao solicitados na entrada para validacao dos ingressos.) \n\nInteiro: ");
+    scanf("%d", &ingInteiro);
+    printf("\n(Valido para estudantes, criancas de 02 a 12 anos, adultos a partir de 60 anos e professores da rede publica de ensino.)\nMeia-entrada: ");
+    scanf("%d", &ingMeia);
+    if(descontoTer == 1){
+        printf("\n\n(Somente valido para criancas carentes da rede publica de ensino.)\nGratuito: ");
+        scanf("%d", &ingTer);
+    }
+// Condicao para contar o total de ingressos quando eh Terca-feira e quando nao eh.
+if (descontoTer == 1){
+    totalIngressos = totalIngressos+ingInteiro+ingMeia+ingTer;
+}
+else{
+    totalIngressos = totalIngressos+ingInteiro+ingMeia;
+}
+    return reservarLugares();
+}
+//Array com as posicoes das cadeiras
+char c[10]={1,2,3,4,5,6,7,8,9,10};
+//Funcao para aparecer os lugares disponiveis atualizados
+void mostrarLugares(){
+    printf("\n\n(%d)  (%d)  (%d)  (%d)  (%d)\n\n(%d)  (%d)  (%d)  (%d)  (%d)\n\n", c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7], c[8], c[9]);
+}
+//Variavel global para guardar o lugar que o usuario digitou
+    int inputLugar;
+//Funcao para reservar lugar
+void reservarLugares(){
+    int i;
+    system("cls");
+    printf(" - Lugares DIsponiveis - ");
+    mostrarLugares();
+    printf("Digite o lugar que deseja reservar(digite 99 se quiser sair): ");
+    scanf("%i", &i);
+    c[i-1]=0;
+    system("cls");
+    if(i==99){
+        return ticket();
+    }
+    else if(i != 99){
+        inputLugar = i;
+    }
+    else{}
+    return reservarLugares();
+}
+//Funcao para aparecer o ingresso impresso
+void ticket(){
+    printf("\nEste ingresso foi enviado para o E-mail: %s\n\nObrigado pela compra, tenha um otimo espetaculo!\n\n*************************\n\nNome: %s\nData: \nCadeira(s): %d\n\n*************************", email, nome, inputLugar);
+}
+
+
+
+
+// CONTINUAR OS DIAS DA SEMANA E ALTERAR PARA QUE O USUARIO COMPRE APENAS 1 INGRESSO DE CADA VEZ.
+
+
+
+
